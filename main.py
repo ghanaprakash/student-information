697c1ef8f0bf8d5e442746fe2161610359a2f9f3
ghana prakash <prakashkaran077@gmail.com>
2024-09-02 08:52:13 +0530

    complete the registration

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app=Flask(__name__)
 def index():
     if request.method=="POST":
         registration(request.form["username"],request.form["age"],request.form["address"],request.form["course"],request.form["duration"])
-        
-    return render_template("stud_mang.html")
+    data=json_read()
+    return render_template("stud_mang.html",student=data["student"])
 
 if __name__=="__main__":
     app.run(debug=True)
